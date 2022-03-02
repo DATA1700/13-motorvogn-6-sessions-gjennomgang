d7a594626bc3533c46e952a724dfc64035d81dff
Erik Storås Sommer <erikssommer@gmail.com>
2022-03-02 17:01:11 +0100

    Starting point for lecture

---
+++ src/test/java/oslomet/webprog/ukeoppgaver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.webprog.ukeoppgaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
