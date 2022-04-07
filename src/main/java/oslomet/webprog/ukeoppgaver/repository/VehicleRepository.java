12fe9c8e61c116fc3d61906b4cb52673926548e5
ulsin <ulriksin@gmail.com>
2022-04-07 09:02:26 +0200

    🧹 CLEAN-UP: fjernet kommentarer i repository

--- src/main/java/oslomet/webprog/ukeoppgaver/repository/VehicleRepository.java
+++ src/main/java/oslomet/webprog/ukeoppgaver/repository/VehicleRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import oslomet.webprog.ukeoppgaver.model.Car;
 import oslomet.webprog.ukeoppgaver.model.Registration;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -16,30 +15,7 @@ public class VehicleRepository {
     @Autowired
     private JdbcTemplate db;
 
-//    private final List<Registration> registrations = new ArrayList<>();
-//    private final List<Car> cars = new ArrayList<>();
-
-//    public VehicleRepository() {
-//        Car car1 = new Car("Volvo","V30");
-//        cars.add(car1);
-//        Car car2 = new Car("Volvo","V70");
-//        cars.add(car2);
-//        Car car3 = new Car("Volvo","V91");
-//        cars.add(car3);
-//        Car car4 = new Car("Audi","A8");
-//        cars.add(car4);
-//        Car car5 = new Car("Audi","Q7");
-//        cars.add(car5);
-//        Car car6 = new Car("Audi","Q8");
-//        cars.add(car6);
-//        Car car7 = new Car("Toyota","Trueno");
-//        cars.add(car7);
-//        Car car8 = new Car("Toyota","Levin");
-//        cars.add(car8);
-//    }
-
     public void addRegistrations(Registration registration){
-//        registrations.add(registration);
         try {
             db.update("insert into Registrations(ssn, name, address, characteristics, brand, type)" +
                     "values(?, ?, ?, ?, ?, ?)",
@@ -52,7 +28,6 @@ public class VehicleRepository {
     }
 
     public List<Registration> getRegistrations(){
-//        return registrations;
         try {
             return db.query("select * from Registrations", new BeanPropertyRowMapper<>(Registration.class));
         } catch (Exception e) {
@@ -62,7 +37,6 @@ public class VehicleRepository {
     }
 
     public List<Car> getCars() {
-//        return cars;
         try {
             return db.query("select * from Cars", new BeanPropertyRowMapper<>(Car.class));
         } catch (Exception e) {
@@ -72,7 +46,6 @@ public class VehicleRepository {
     }
 
     public void deleteVehicles(){
-//        registrations.clear();
         try {
             db.update("delete from Registrations");
         } catch (Exception e) {
