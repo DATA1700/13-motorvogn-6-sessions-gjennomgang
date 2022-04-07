24ec2228e21a55f8f2aceec5dd609c5fbbc4fd47
ulsin <ulriksin@gmail.com>
2022-04-07 09:01:04 +0200

    lagde filer for database setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+insert into Cars(brand, type)
+values  ('Volvo','V30'),
+        ('Volvo','V70'),
+        ('Volvo','V91'),
+        ('Audi','A8'),
+        ('Audi','Q7'),
+        ('Audi','Q8'),
+        ('Toyota','Trueno'),
+        ('Toyota','Corolla'),
+        ('Toyota','Levin');
+
+-- demo to debug button
+insert into Registrations(ssn, name, address, characteristics, brand, type)
+values   ('12345678901', 'Ulrik', 'OsloMet', 'AB12345', 'Toyota', 'Corolla'),
+         ('09876543210', 'Ulrik', 'OsloMet', 'AB12345', 'Toyota', 'Corolla');
